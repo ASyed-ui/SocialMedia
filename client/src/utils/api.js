d3ce98e3ab3264639027a857b778cf7a6e52aab0
ASyed-ui <aadil.syed2@gmail.com>
2025-12-07 18:08:34 -0500

    fixed pointer to backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@
 // -----------------------------
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5001/api';
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://socialmedia-kpk8.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
